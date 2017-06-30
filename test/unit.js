ff019923b9a630bfbf31cd569f93fa7403d7fe63
Eric Martindale <eric@ericmartindale.com>
2017-06-29 17:37:13 -0700

    Stub out peer messaging

--- test/unit.js
+++ test/unit.js
@@ -1,20 +1,38 @@
 var assert = require('assert');
 var expect = require('chai').expect;
 
+var genesis = require('../data/fabric');
+var message = require('../data/message');
+
 var Fabric = require('../');
-var fabric = new Fabric({});
+var fabric = new Fabric(genesis);
 
 // test our own expectations.  best of luck.
 // @consensus:
 // @quest:
 // > *Warning:* ahead lies death. # must be attributed to "Game Master"
 //
-describe('Fabric', function () {
-  it('should expose a constructor', function () {
+describe('Fabric', function() {
+  it('should expose a constructor', function() {
     assert(typeof Fabric, 'function');
   });
 
-  it('has the correct, hard-coded genesis seed', function provenance () {
-    assert.equal(fabric.id, 0); // require a point of origin.
+  it('has the correct, hard-coded genesis seed', function provenance() {
+    assert.equal(fabric.root.id, 0); // require a point of origin.
+    assert.equal(fabric.root.id, genesis.root.id);
+  });
+
+  it('can acknowledge its own existence', function identity() {
+    var alice = new Fabric(genesis);
+    var bob = new Fabric(genesis);
+
+    alice.peers.push(bob);
+
+    assert.equal(bob.root.id, alice.peers[0].root.id);
+  });
+
+  it('can send a message', function broadcast() {
+
+    fabric.broadcast()
   });
 });
