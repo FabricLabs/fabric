9dc5a689c3a2e1c4a9fdf322d3b2c0f3e8c5b598
Eric Martindale <eric@ericmartindale.com>
2017-11-09 18:30:58 -0800

    Add new Remote handler
    TODO: merge this into maki-remote

---
+++ test/remote.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+
+var fs = require('fs');
+
+var Remote = require('../lib/remote');
+
+var key = '';
+
+describe('Remote', function () {
+  it('should expose a constructor', function () {
+    assert.equal(typeof Remote, 'function');
+  });
+
+  it('can emulate HTTP GET', async function () {
+    var remote = new Remote({
+      host: 'example.com'
+    });
+    var result = await remote._GET(key);
+
+    assert.ok(result);
+  });
+
+});
