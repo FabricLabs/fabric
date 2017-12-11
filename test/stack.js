1b13cf4dd5df7369e14fd233aa3bd6a8d267d165
Eric Martindale <eric@ericmartindale.com>
2017-12-10 22:20:31 -0800

    Introduce computational tests

--- test/stack.js
+++ test/stack.js
@@ -18,4 +18,24 @@ describe('Fabric.stack', function () {
     assert.equal(fabric['@data'], true);
     assert.equal(fabric.clock, 1);
   });
+
+  it('can add two numbers', function () {
+    var fabric = new Fabric();
+
+    fabric.use('ADD', function (state) {
+      var op = this.stack.pop();
+      var a = this.stack.pop();
+      var b = this.stack.pop();
+      return parseInt(a) + parseInt(b);
+    });
+
+    fabric.stack.push('1');
+    fabric.stack.push('1');
+    fabric.stack.push('ADD');
+
+    fabric.compute();
+
+    assert.equal(fabric['@data'], 2);
+    assert.equal(fabric.clock, 1);
+  });
 });
