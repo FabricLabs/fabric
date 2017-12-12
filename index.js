5963d359269fc5eda5d3687e092b8a35d810ad49
Eric Martindale <eric@ericmartindale.com>
2017-12-11 21:12:33 -0800

    Expose Datastore in public API

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var Fabric = require('./lib/fabric');
 
 Fabric.App = require('./lib/app');
 Fabric.Chain = require('./lib/chain');
+Fabric.Datastore = require('./lib/datastore');
 Fabric.Machine = require('./lib/machine');
 Fabric.Oracle = require('./lib/oracle');
 Fabric.Remote = require('./lib/remote');
