c3346ec0618cfae7a9c020e97f4b4544b3898a3b
Eric Martindale <eric@ericmartindale.com>
2017-12-17 18:57:14 -0800

    Bundle various changes, update blockchain example

--- examples/blockchain.js
+++ examples/blockchain.js
@@ -1,15 +1,63 @@
+'use strict';
+
 var Block = require('../lib/block');
 var Chain = require('../lib/chain');
 
 var genesis = require('../data/block');
 
 var chain = new Chain();
-var block = new Block(genesis);
+var max = 2;
 
-chain.append(block);
+chain.on('candidate', async function (block) {
+  console.log('chain received candidate block:', block);
 
-chain.mine();
+  var candidate = new Block(block['@data']);
+  var isValid = candidate.validate();
 
-chain.compute();
+  candidate.compute();
 
-console.log('chain:', chain);
+  console.log('isValid:', isValid);
+
+  if (!isValid) {
+    // TODO: disconnect peers
+    return false;
+  }
+
+  await chain.append(candidate);
+
+  console.log('chain length:', chain['@data'].length);
+
+  if (typeof heapdump !== 'undefined') {
+    heapdump.writeSnapshot(function (err, filename) {
+      console.log('dump written to', filename);
+    });
+  }
+
+  if (chain['@data'].length < max) {
+    await chain.mine();
+  } else {
+    console.log('Chain filled!');
+    console.log('Chain:', chain);
+  }
+});
+
+main();
+
+async function main () {
+  var start = new Block(genesis);
+
+  start.compute();
+
+  await chain.append(start);
+
+  console.log('computing... stack:', chain.stack);
+  chain.compute();
+  console.log('compute done.');
+
+  console.log('starting... chain:', chain['@id']);
+  console.log('mining...');
+
+  await chain.mine();
+
+  console.log('chain id:', chain['@id']);
+}
--- test/app.js
+++ test/app.js
@@ -9,9 +9,16 @@ describe('App', function () {
     assert.equal(typeof Fabric.App, 'function');
   });
 
-  it('should create an application smoothly', function () {
+  it('should create an application smoothly', async function () {
     var app = new Fabric.App();
+
+    await app.tips.close();
+    await app.stash.close();
+
+    console.log('app:', app);
+
     assert.ok(app);
+
   });
 
   it('should load data from an oracle', async function () {
@@ -19,11 +26,18 @@ describe('App', function () {
       path: './data/oracle'
     });
     var app = new Fabric.App();
+    
+    console.log('app:', app);
+    console.log('orace:', oracle);
 
     await oracle._load('./resources');
 
     await app._defer(oracle);
     await app._explore();
+
+    await app.tips.close();
+    await app.stash.close();
+    await oracle.store.close();
 
     assert.ok(oracle);
     assert.ok(app);
