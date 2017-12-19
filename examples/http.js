d3fd1b480e38064535e060887e92ff05f829d8b9
Eric Martindale <eric@ericmartindale.com>
2017-12-18 18:49:58 -0800

    Add basic Resources to demo HTTP

--- examples/http.js
+++ examples/http.js
@@ -1,5 +1,17 @@
-var Fabric = require('../');
+'use strict'
 
+var Fabric = require('../');
 var server = new Fabric.HTTP();
+
+server.define('Person', {
+  name: 'Person',
+  properties: {
+    username: { type: String , maxLength: 55 }
+  },
+  routes: {
+    query: '/people',
+    get: '/people/:id'
+  }
+});
 
 server.start();
