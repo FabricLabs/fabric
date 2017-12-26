e2e49e26eec0e2ae4ac7711585df582974cd543b
Eric Martindale <eric@ericmartindale.com>
2017-12-25 17:33:44 -0800

    Add test coverage for HTTP server

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ Fabric.HTTP = require('./lib/http');
 Fabric.Machine = require('./lib/machine');
 Fabric.Oracle = require('./lib/oracle');
 Fabric.Remote = require('./lib/remote');
+Fabric.Transaction = require('./lib/transaction');
 Fabric.Validator = require('./lib/validator');
 Fabric.Vector = require('./lib/vector');
 Fabric.Walker = require('./lib/walker');
---
+++ test/http.js
@@ -0,0 +1,49 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+
+var Fabric = require('../');
+var server = new Fabric.HTTP();
+
+describe('HTTP', function () {
+  before(function (done) {
+    server.define('Widget', {
+      properties: {
+        name: { type: String , maxLength: 100 }
+      },
+      routes: {
+        query: '/widgets',
+        get: '/widgets/:id'
+      }
+    });
+
+    server.start(done);
+  });
+
+  after(async function () {
+    await server.stop();
+  });
+
+  it('should expose a constructor', function () {
+    assert.equal(typeof Fabric.HTTP, 'function');
+  });
+
+  it('can serve a resource', async function () {
+    let remote = new Fabric.Remote({
+      host: 'localhost:3000'
+    });
+
+    let payload = {
+      name: 'foobar'
+    };
+
+    let vector = new Fabric.Vector(payload);
+
+    vector._sign();
+
+    let result = await remote._POST('/widgets', payload);
+    let object = await remote._GET('/widgets');
+
+    assert.equal(result['@id'], vector['@id']);
+    assert.equal(object[0]['@id'], vector['@id']);
+  });
+});
