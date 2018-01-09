7f4f31e6f625f536e233f753fdf1261453f7b3bb
Eric Martindale <eric@ericmartindale.com>
2018-01-09 14:57:05 -0800

    Add components for CLI, simple chat interface

--- examples/cli.js
+++ examples/cli.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('debug-trace')({ always: true });
+
 //const Oracle = require('./oracle');
 const CLI = require('../lib/cli');
 
@@ -8,7 +10,10 @@ async function main () {
     ui: './assets/cli.jade'
   });
 
-  cli.start();
+  cli.start(function () {
+    //cli.viewer.document.page[0].item[0].donut[0].data[0].m[0]['$'].percent = 50;
+    //cli.viewer.renderPage(0);
+  });
 }
 
 main();
