9b64cd8aa6892e4987bc29ca31ad045b2790c438
Eric Martindale <eric@ericmartindale.com>
2018-01-11 17:20:05 -0800

    Clean up Remote tests

--- test/remote.js
+++ test/remote.js
@@ -4,38 +4,41 @@ var expect = require('chai').expect;
 var Remote = require('../lib/remote');
 var HTTP = require('../lib/http');
 
-var server = new HTTP();
 var index = '/';
 
 describe('Remote', function () {
-  before(async function () {
-    await server.start();
-  });
-
-  after(async function () {
-    await server.stop();
-  });
-
   it('should expose a constructor', function () {
     assert.equal(typeof Remote, 'function');
   });
 
   it('can emulate HTTP GET', async function () {
-    var remote = new Remote({
+    let server = new HTTP();
+    let remote = new Remote({
       host: 'localhost:3000',
       secure: false
     });
-    var result = await remote._GET(index);
+
+    await server.start();
+
+    let result = await remote._GET(index);
+
+    await server.stop();
 
     assert.ok(result);
   });
 
   it('can emulate HTTP OPTIONS', async function () {
-    var remote = new Remote({
+    let server = new HTTP();
+    let remote = new Remote({
       host: 'localhost:3000',
       secure: false
     });
-    var result = await remote._OPTIONS(index);
+
+    await server.start();
+    
+    let result = await remote._OPTIONS(index);
+
+    await server.stop();
 
     assert.ok(result);
   });
