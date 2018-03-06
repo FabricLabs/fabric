ef9e355a21f76c018ed882b4a9c473cd126de27c
Eric Martindale <eric@ericmartindale.com>
2018-03-06 07:42:37 -0800

    General Resource flow and tests

--- test/resource.js
+++ test/resource.js
@@ -6,6 +6,12 @@ const assert = require('assert');
 const expect = require('chai').expect;
 
 const widget = require('../data/widget');
+const update = {
+  name: 'updated!'
+};
+const last = {
+  name: 'finally!'
+};
 
 describe('Resource', function () {
   it('should expose a constructor', function () {
@@ -27,6 +33,7 @@ describe('Resource', function () {
       assert.ok(test);
     } catch (E) {
       console.error(E);
+      assert.fail(E);
     }
 
     await store.close();
@@ -37,7 +44,6 @@ describe('Resource', function () {
     let store = new Fabric.Store();
 
     await store.open();
-
 
     try {
       await resource.trust(store);
@@ -55,6 +61,7 @@ describe('Resource', function () {
       assert.equal(results.length, 1);
     } catch (E) {
       console.error(E);
+      assert.fail(E);
     }
 
     await store.close();
@@ -82,6 +89,115 @@ describe('Resource', function () {
       assert.equal(results.length, 1);
     } catch (E) {
       console.error(E);
+      assert.fail(E);
+    }
+
+    await store.close();
+  });
+
+  it('should call get() smoothly', async function () {
+    let resource = new Fabric.Resource(widget);
+    let store = new Fabric.Store();
+
+    await store.open();
+
+    try {
+      await resource.trust(store);
+      await resource.flush();
+
+      let test = await resource.create({
+        name: 'Wobbler'
+      });
+
+      let collection = await resource.query();
+      let instance = await resource.get(test.id);
+
+      assert.ok(test);
+      assert.ok(collection);
+      assert.ok(instance);
+
+      assert.equal(collection.length, 1);
+      assert.equal(instance.id, test.id);
+    } catch (E) {
+      console.error(E);
+      assert.fail(E);
+    }
+
+    await store.close();
+  });
+
+  it('should call update() smoothly', async function () {
+    let resource = new Fabric.Resource(widget);
+    let store = new Fabric.Store();
+
+    await store.open();
+
+    try {
+      await resource.trust(store);
+      await resource.flush();
+
+      let test = await resource.create({
+        name: 'Wobbler'
+      });
+
+      let collection = await resource.query();
+      let instance = await resource.get(test.id);
+
+      assert.ok(test);
+      assert.ok(collection);
+      assert.ok(instance);
+
+      assert.equal(collection.length, 1);
+      assert.equal(instance.id, test.id);
+
+      let result = await resource.update(test.id, update);
+      let after = await resource.get(result.id);
+
+      assert.equal(after['@data'].name, update.name);
+    } catch (E) {
+      console.error(E);
+      assert.fail(E);
+    }
+
+    await store.close();
+  });
+
+  it('should call update() multiple times and resolve output', async function () {
+    let resource = new Fabric.Resource(widget);
+    let store = new Fabric.Store();
+
+    await store.open();
+
+    try {
+      await resource.trust(store);
+      await resource.flush();
+
+      let test = await resource.create({
+        name: 'Wobbler'
+      });
+
+      let collection = await resource.query();
+      let instance = await resource.get(test.id);
+
+      assert.ok(test);
+      assert.ok(collection);
+      assert.ok(instance);
+
+      assert.equal(collection.length, 1);
+      assert.equal(instance.id, test.id);
+
+      let result = await resource.update(test.id, update);
+      let after = await resource.get(result.id);
+
+      assert.equal(after['@data'].name, update.name);
+
+      let again = await resource.update(test.id, last);
+      let final = await resource.get(again.id);
+
+      assert.equal(final['@data'].name, last.name);
+    } catch (E) {
+      console.error(E);
+      assert.fail(E);
     }
 
     await store.close();
