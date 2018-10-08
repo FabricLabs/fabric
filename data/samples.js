30921e8a14e670842a09ca0c84640654b95cc246
Eric Martindale <eric@ericmartindale.com>
2018-10-08 12:23:51 -0700

    Add basic Chain definition

--- data/samples.js
+++ data/samples.js
@@ -8,7 +8,7 @@ class NamedCollections {
     return {
       'array': '4f53cda18c2baa0c0354bb5f9a3ecbe5ed12ab4d8e11ba873c2f11161202b945',
       'empty': '44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a',
-      'fabric': '71b4b2f3376f3e5ca9f2642335811aead89dc30f0f327727695227599cd3a503',
+      'fabric': '1c866bf97f233de116bb311f47c9a432f2c3d5efc944dda1f33ff4b8e24313e3',
       'object': '44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a',
       'sample': '156403d8f795a18e92f5d4377c84f49bdb321700289f1ab01ee7c58e7c994317',
       'stackWithSingleValidFrame': '1c28f2eb0958c3d15db1f0f0e7f2b8998ca2b8f67ab426a1fbb3d561fe76fad9'
@@ -21,7 +21,7 @@ class NamedCollections {
       '1c28f2eb0958c3d15db1f0f0e7f2b8998ca2b8f67ab426a1fbb3d561fe76fad9': '[true]',
       '44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a': '{}',
       '4f53cda18c2baa0c0354bb5f9a3ecbe5ed12ab4d8e11ba873c2f11161202b945': '[]',
-      '71b4b2f3376f3e5ca9f2642335811aead89dc30f0f327727695227599cd3a503': JSON.stringify(fabric['@data'])
+      '1c866bf97f233de116bb311f47c9a432f2c3d5efc944dda1f33ff4b8e24313e3': JSON.stringify(fabric['@data'])
     };
   }
 
