ed04783bdd7935486b53bba1fb4ad1fba007c529
Eric Martindale <eric@ericmartindale.com>
2019-04-27 21:51:29 -0700

    Fix elements in CLI

--- constants.js
+++ constants.js
@@ -6,6 +6,8 @@
  */
 'use strict';
 
+const MAX_PEERS = 32;
+
 const MAGIC_BYTES = 0xC0D3F33D;
 const VERSION_NUMBER = 0x01;
 const BITCOIN_GENESIS = 0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f;
@@ -18,6 +20,7 @@ const MAX_FRAME_SIZE = 32; // max size of a stack frame in bytes
 const MAX_MEMORY_ALLOC = MAX_STACK_HEIGHT * MAX_FRAME_SIZE;
 
 module.exports = {
+  MAX_PEERS,
   BITCOIN_GENESIS,
   HEADER_SIZE,
   MAGIC_BYTES,
