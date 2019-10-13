d2e2ed9eeba314ddfe3b41451dc918629306525c
Eric Martindale <eric@ericmartindale.com>
2019-10-13 14:12:33 -0700

    Allows Seed restoration (!), exposes Wallet API

--- types/wallet.js
+++ types/wallet.js
@@ -8,10 +8,12 @@ const State = require('./state');
 // Bcoin
 const bcoin = require('bcoin/lib/bcoin-browser').set('testnet');
 // Convenience classes...
+const Address = bcoin.Address;
 const Coin = bcoin.Coin;
 const WalletDB = bcoin.WalletDB;
 const WalletKey = bcoin.wallet.WalletKey;
 const Outpoint = bcoin.Outpoint;
+const Output = bcoin.Output;
 const Keyring = bcoin.wallet.WalletKey;
 const Mnemonic = bcoin.hd.Mnemonic;
 const HD = bcoin.hd;
@@ -31,8 +33,9 @@ class Wallet extends Service {
 
     this.settings = Object.assign({
       name: 'default',
-      network: 'testnet',
+      network: 'regtest',
       language: 'english',
+      witness: false,
       key: null
     }, settings);
 
@@ -45,6 +48,7 @@ class Wallet extends Service {
     this.manager = null;
     this.wallet = null;
     this.master = null;
+    this.ring = null;
     this.seed = null;
     this.key = null;
 
@@ -53,13 +57,21 @@ class Wallet extends Service {
     this.index = 0;
 
     this.accounts = new Collection();
+    this.coins = new Collection();
 
+    // Internal State
+    this._state = {
+      coins: []
+    };
+
+    // External State
     this.state = {
       asset: null,
       balances: {
         confirmed: 0,
         unconfirmed: 0
       },
+      coins: [],
       transactions: []
     };
 
@@ -76,7 +88,12 @@ class Wallet extends Service {
     return this.get('/transactions');
   }
 
+  getAddressFromRedeemScript (redeemScript) {
+    return Address.fromScripthash(redeemScript.hash160());
+  }
+
   async _createAccount (data) {
+    console.log('wallet creating account with data:', data);
     return this.wallet.createAccount(data);
   }
 
@@ -89,7 +106,7 @@ class Wallet extends Service {
   }
 
   _getDepositAddress () {
-    return this.address;
+    return this.ring.getAddress().toString();
   }
 
   _getSeed () {
@@ -102,39 +119,133 @@ class Wallet extends Service {
     };
   }
 
-  async _getFreeCoinbase (amount) {
+  async _getFreeCoinbase (amount = 1) {
+    await this._load();
+
     const coins = {};
     const coinbase = new MTX();
 
-    // Add a typical coinbase input
+    // INSERT 1 Input
     coinbase.addInput({
       prevout: new Outpoint(),
-      script: new Script()
+      script: new Script(),
+      sequence: 0xffffffff
     });
 
+    // INSERT 1 Output
     coinbase.addOutput({
       address: this._getDepositAddress(),
       value: amount * 100000000 // amount in Satoshis
     });
 
-    // Convert the coinbase output to a Coin
-    // object and add it to the available coins for that keyring.
-    // In reality you might get these coins from a wallet.
-    coins[0] = Coin.fromTX(coinbase, 0, -1);
-    console.log('coins:', coins);
+    // TODO: wallet._getSpendableOutput()
+    let coin = Coin.fromTX(coinbase, 0, -1);
 
-    return coins[0];
+    this._state.coins.push(coin);
+
+    console.log('coins:', this._state.coins);
+    
+    return this._state.coins[0];
   }
 
+  async _sign (tx) {
+    let signature = await tx.sign(this.keyring);
+    console.log('signing tx:', tx);
+    console.log('signing sig:', signature);
+    return Object.assign({}, tx, { signature });
+  }
 
-  async _getSpendableOutput (amount) {
-    let key = null;
+  async _createIncentivizedTransaction (config) {
+    console.log('creating incentivized transaction with config:', config);
+
     let mtx = new MTX();
-    let address = ``;
+    let data = new Script();
 
-    mtx.addOutput({ value: amount, address: `${address}` });
+    data.pushSym('OP_IF');
+    data.pushSym('OP_SHA256');
+    data.pushData(hash);
+    data.pushSym('OP_EQUALVERIFY');
+    data.pushData(swapPubkey);
+    data.pushSym('OP_CHECKSIG');
+    data.pushSym('OP_ELSE');
+    data.pushInt(locktime);
+    data.pushSym('OP_CHECKSEQUENCEVERIFY');
+    data.pushSym('OP_DROP');
+    data.pushData(refundPubkey);
+    data.pushSym('OP_CHECKSIG');
+    data.pushSym('OP_ENDIF');
+    data.compile();
+
+    console.log('address data:', data);
+
+    mtx.addOutput({
+      address: data,
+      value: 0
+    });
+
+    // TODO: load available outputs from wallet
+    let out = await mtx.fund([] /* coins */, {
+      // TODO: fee estimation
+      rate: 10000,
+      changeAddress: this.ring.getAddress()
+    });
+
+
+
+    console.log('transaction:', out);
+    return out;
+  }
+
+  async _getBondAddress () {
+    await this._load();
+
+    let script = new Script();
+    let clean = await this.generateCleanKeyPair();
+
+    console.log('[AUDIT]', 'getting bond address, clean:', clean);
+
+    // write the contract
+    // script.pushData(clean.public.toString('hex'));
+    // script.pushSym('OP_CHECKSIG');
+
+    // compile the script
+    // script.compile();
 
     return {
+      pubkey: clean.public.toString(),
+      address: clean.address
+    };
+  }
+
+  async _getSpendableOutput (target, amount = 0) {
+    let self = this;
+    let key = null;
+    let out = null;
+    let mtx = new MTX();
+
+    await this._load();
+
+    console.log('funding transaction with coins:', this._state.coins);
+
+    // INSERT 1 Output
+    mtx.addOutput({
+      address: target,
+      value: amount
+    });
+
+    out = await mtx.fund(this._state.coins, {
+      // TODO: fee estimation
+      rate: 10000,
+      changeAddress: self.ring.getAddress()
+    });
+
+    console.log('out:', out);
+
+    console.trace('created mutable transaction:', mtx);
+    console.trace('created immutable transaction:', mtx.toTX());
+
+    return {
+      tx: mtx.toTX(),
       mtx: mtx
     };
   }
@@ -143,11 +254,14 @@ class Wallet extends Service {
     if (this.status !== 'loaded') await this._load();
 
     this.index++;
+
     let key = this.master.derivePath(`m/44/0/0/0/${this.index}`);
-    let keyring = KeyRing.fromPrivate(key.privateKey);
+    let keyring = bcoin.KeyRing.fromPrivate(key.privateKey);
+
     return {
       index: this.index,
-      public: keyring.publicKey
+      public: keyring.publicKey.toString('hex'),
+      address: keyring.getAddress('string')
     };
   }
 
@@ -162,9 +276,6 @@ class Wallet extends Service {
 
   async _registerAccount (obj) {
     if (!obj.name) throw new Error('Account must have "name" property.');
-
-    this.status = 'creating';
-
     if (!this.database.db.loaded) {
       await this.database.open();
     }
@@ -190,49 +301,64 @@ class Wallet extends Service {
     return this.database.close();
   }
 
+  /**
+   * Initialize the wallet, including keys and addresses.
+   * @param {Object} settings 
+   */
   async _load (settings = {}) {
+    if (this.wallet) return this;
+
     this.status = 'loading';
+    this.master = null;
 
-    await this.database.open();
+    if (!this.database.db.loaded) {
+      await this.database.open();
+    }
 
-    let master = null;
-
-    try {
-      if (this.settings.key) {
-        let mnemonic = new Mnemonic(this.settings.key.seed);
-        master = bcoin.hd.fromMnemonic(mnemonic);
-      }
-    } catch (E) {
-      console.error('Could not find/restore key:', E);
+    if (this.settings.key && this.settings.key.seed) {
+      let mnemonic = new Mnemonic(this.settings.key.seed);
+      this.master = bcoin.hd.fromMnemonic(mnemonic);
+    } else {
+      this.master = bcoin.hd.from(mnemonic, this.settings.network);
     }
 
     this.wallet = await this.database.create({
-      name: 'default',
+      name: this.settings.name,
       network: this.settings.network,
-      master: master
+      master: this.master
     });
 
-    this.account = await this.wallet.getAccount('default');
-    this.address = await this.account.receiveAddress();
-    this.master = master;
+    // Setup Ring
+    this.ring = new bcoin.KeyRing(this.master, this.settings.network);
+    this.ring.witness = this.settings.witness;
 
-    /* this.account.bind('confirmed', async function (wallet, transaction) {
-      console.log('wallet confirmed:', wallet, transaction);
+    console.log('keyring:', this.ring);
+    console.log('address from keyring:', this.ring.getAddress().toString());
+
+    // TODO: notify downstream of short-circuit removal
+    this.account.bind('confirmed', async function (wallet, transaction) {
+      console.log('[AUDIT]', 'wallet confirmed:', wallet, transaction);
       self.emit('confirmation', {
         '@type': 'Confirmation',
-        '@data': transaction
+        '@data': { transaction }
       });
-    }); */
+    });
+
+    this.account = await this.wallet.getAccount(this.settings.name);
+    // TODO: also retrieve key for address
+    // let key = this.master.derivePath('m/44/0/0/0/0');
+    // TODO: label as identity address
+    // this.address = await this.account.receiveAddress();
 
     this.status = 'loaded';
-
     this.emit('ready');
-
-    // console.log('[FABRIC:WALLET]', 'Wallet opened:', this.wallet);
 
     return this;
   }
 
+  /**
+   * Start the wallet, including listening for transactions.
+   */
   async start () {
     return this._load();
   }
