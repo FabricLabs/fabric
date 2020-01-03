06809638dc00df5272d6db8ade97b0dba5cee9e4
Eric Martindale <eric@ericmartindale.com>
2020-01-03 18:58:42 -0500

    Clean up tests

--- tests/fabric.wallet.js
+++ tests/fabric.wallet.js
@@ -10,27 +10,5 @@ describe('@fabric/core/types/wallet', function () {
     it('is available from @fabric/core', function () {
       assert.equal(Wallet instanceof Function, true);
     });
-
-    it('can use _SET', async function () {
-      let wallet = new Wallet();
-
-      await wallet.start();
-      await wallet._SET('sample', message['@data']);
-      await wallet.stop();
-
-      assert.ok(wallet);
-    });
-
-    xit('can store a string value', async function () {
-      let wallet = new wallet();
-
-      await wallet.start();
-      let set = await wallet._SET('sample', message['@data']);
-      let get = await wallet._GET('sample');
-      await wallet.stop();
-
-      assert.ok(wallet);
-      assert.equal(typeof get, 'string');
-    });
   });
 });
