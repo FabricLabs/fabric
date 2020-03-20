c24f13730fe277631413c775dc0c7b75de8a7704
Eric Martindale <eric@ericmartindale.com>
2020-03-20 16:50:03 -0400

    Update classtree.js

--- scripts/classtree.js
+++ scripts/classtree.js
@@ -1,71 +1,53 @@
-let fs = require('fs');
+'use strict';
 
-let path = process.argv[2];
+const fs = require('fs');
+const path = process.argv[2];
 
-var files = fs.readdirSync(path);
-
-//console.log("FILES", files);
-
+let files = fs.readdirSync(path);
 let classExtends = {};
 
-for(var i = 0; i<files.length; i++){
-  if(!files[i].includes('.js')) continue;
-
-
+for (let i = 0; i < files.length; i++) {
+  if (!files[i].includes('.js')) continue;
   let text = fs.readFileSync(path + "/" + files[i], 'utf8');
-
-  //console.log(text)
   let ci = text.indexOf('class ');
 
-  let decl = "";
-  if(ci>=0){
-    for(var j=ci; j<text.length; j++){
-      if(text[j] == '{') break;
-
-      decl+=text[j];
+  let decl = '';
+  if (ci >= 0) {
+    for (var j = ci; j < text.length; j++) {
+      if (text[j] === '{') break;
+      decl += text[j];
     }
   }
 
   decl = decl.trim();
-  if(!decl) continue;
+  if (!decl) continue;
 
   let parts = decl.split(' ');
-  if(parts.length > 4) continue;
-
-  //let expr = /class ([0-9]*?(?=-)-[0-9]*)/;;
-  //console.log("-".repeat(80))
-  //console.log(files[i])
-
-  //console.log(parts);
+  if (parts.length > 4) continue;
 
   let className = parts[1];
   let extendsClassName = null;
 
-  if(parts.includes('extends')){
+  if (parts.includes('extends')) {
     let classPath = parts[3].split('.');
-    extendsClassName = classPath[classPath.length-1];
+    extendsClassName = classPath[classPath.length - 1];
   }
 
   classExtends[className] = extendsClassName;
-  if(!classExtends[extendsClassName]) classExtends[extendsClassName] = null;
+  if (!classExtends[extendsClassName]) classExtends[extendsClassName] = null;
 }
 
 classExtends['EventEmitter'] = null;
-//console.log(classExtends);
 
-let tree = {};
-
-function buildSubtree(subtree, parent=null){
-  for(var k in classExtends){
-    if(k=="null" || k==null) continue;
-    if(classExtends[k] == parent){
+function buildSubtree(subtree, parent=null) {
+  for (let k in classExtends) {
+    if (k === 'null' || k === null) continue;
+    if (classExtends[k] === parent) {
       subtree[k] = {};
-
       buildSubtree(subtree[k], k);
     }
   }
 }
 
-buildSubtree(tree)
-
-console.log(JSON.stringify(tree, null,2))
+buildSubtree(tree);
+console.log(JSON.stringify(tree, null, 2));
