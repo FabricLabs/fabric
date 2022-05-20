abb603bbbb6eb7184530c77f6394b20f74f26e8d
Anand Suresh <anandsuresh@gmail.com>
2022-05-19 22:55:50 -0700

    remove unnececssary builds/attributes

--- node-packages.nix
+++ node-packages.nix
@@ -15786,21 +15786,6 @@ let
   };
 in
 {
-  args = args;
-  sources = sources;
-  tarball = nodeEnv.buildNodeSourceDist args;
   package = nodeEnv.buildNodePackage args;
   shell = nodeEnv.buildNodeShell args;
-  nodeDependencies = nodeEnv.buildNodeDependencies (lib.overrideExisting args {
-    src = stdenv.mkDerivation {
-      name = args.name + "-package-json";
-      src = nix-gitignore.gitignoreSourcePure [
-        "*"
-        "!package.json"
-        "!package-lock.json"
-      ] args.src;
-      dontBuild = true;
-      installPhase = "mkdir -p $out; cp -r ./* $out;";
-    };
-  });
 }
