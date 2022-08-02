712c8d93763fc962651d7d4058739455fd058719
Eric Martindale <eric@ericmartindale.com>
2022-08-02 00:32:27 -0400

    Update Environment tests

--- tests/fabric.environment.js
+++ tests/fabric.environment.js
@@ -10,20 +10,41 @@ describe('@fabric/core/types/environment', function () {
       assert.equal(Environment instanceof Function, true);
     });
 
+    it('can start and stop smoothly', async function () {
+      const environment = new Environment();
+      await environment.start();
+      await environment.stop()
+      assert.ok(environment);
+    });
+
+    it('can read an environment variable', async function () {
+      const environment = new Environment();
+      const home = environment.readVariable('HOME');
+      assert.ok(home);
+    });
+
     it('can check for store', async function () {
-      let environment = new Environment();
-      let exists = environment.storeExists();
+      const environment = new Environment();
+      const exists = environment.storeExists();
 
       assert.ok(environment);
       assert.equal(typeof exists, 'boolean');
     });
 
     it('can check for wallet', async function () {
-      let environment = new Environment();
-      let exists = environment.walletExists();
+      const environment = new Environment();
+      const exists = environment.walletExists();
 
       assert.ok(environment);
       assert.equal(typeof exists, 'boolean');
+    });
+
+    it('can touch the wallet', async function () {
+      const environment = new Environment();
+
+      environment.touchWallet();
+
+      assert.ok(environment);
     });
   });
 });
