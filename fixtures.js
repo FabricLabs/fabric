f8424d2017e91a8449f5240b9012f9b04f5f5ff4
Eric Martindale <eric@ericmartindale.com>
2022-10-13 02:47:15 -0400

    Add fixtures.js

---
+++ fixtures.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const TEST_SEED = 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
+
+module.exports = {
+  FABRIC_SEED: TEST_SEED,
+  FABRIC_XPRV: TEST_SEED,
+  FABRIC_XPUB: TEST_SEED,
+  FIXTURE_SEED: TEST_SEED,
+  PLAYNET_SEED: TEST_SEED,
+  TEST_SEED: TEST_SEED,
+  seed: TEST_SEED,
+  xprv: 'xprv9s21ZrQH143K3h3fDYiay8mocZ3afhfULfb5GX8kCBdno77K4HiA15Tg23wpbeF1pLfs1c5SPmYHrEpTuuRhxMwvKDwqdKiGJS9XFKzUsAF',
+  identity: {
+    xpub: 'xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8'
+  }
+};
