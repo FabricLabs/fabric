e4b83cf789a163aa7f39c2cac8a7d75001e73d2b
Eric Martindale <eric@ericmartindale.com>
2023-01-28 12:57:21 -0400

    Fix build helpers, add install log

--- examples/app.js
+++ examples/app.js
@@ -26,7 +26,12 @@
 // ## Browsers
 // This example is intended to be run in the browser.
 if (!window) throw new Error('Not running in browser.  Exiting.');
-
+//
+// Now that you've verified your environment, try the following:
+// ```
+// npm run build
+// ```
+//
 // ## Settings
 // Settings include browser configuration; script paths, console settings, and consensus parameters.
 const settings = {
@@ -43,25 +48,27 @@ const settings = {
 };
 
 // ## Dependencies
+// Import the `App` type.
 const App = require('../types/app');
 
 // ## Resources
+// Resources are named collections.
 const Person = require('../resources/person');
 const Post = require('../resources/post');
 const Relationship = require('../resources/relationship');
 
 // ## Functions
-// Error Handler
+// ### Error Handler
 function _handleError (error) {
   console.error(settings.label, '[ERROR]', error);
 }
 
-// Output Handler
+// ### Output Handler
 function _handleOutput (output) {
   console.log(settings.label, '[READY:OUTPUT]', output);
 }
 
-// Main Process
+// ## Main Process
 async function main (input = {}) {
   // Create and start the Fabric Application
   window.app = new App(input);
@@ -73,29 +80,32 @@ async function main (input = {}) {
   };
 }
 
+// ## Service Workers
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/service.js').then(function(registration) {
-      console.log(settings.label, 'ready: ', registration.scope);
+      console.log(settings.label, 'Service Worker load ready: ', registration.scope);
     }, function(err) {
-      console.log(settings.label, 'failed: ', err);
+      console.log(settings.label, 'Service Worker load failed: ', err);
     });
   });
 }
 
+// ## Bindings
 // Assign our onload function
 window.onload = function () {
   app.attach(document.querySelector('fabric-application'));
-  //app._load();
+  //app._load(); // loads state into app
 
   // TODO: document defer as trust(authority) with Remote class
   /*/
-  app._defer('localhost:3000');
+  app._defer('localhost:3000'); // use localhost
   /*/
-  app._defer('maki.io');
+  app._defer('maki.io'); // use maki.io
   /**/
 
   app._explore();
 }
 
+// ## Execution
 main(settings).catch(_handleError).then(_handleOutput);
