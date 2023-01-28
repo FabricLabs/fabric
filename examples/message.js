5d5baf5f4b4028348a87f75647fd4368675294a3
Eric Martindale <eric@ericmartindale.com>
2023-01-28 01:08:03 -0400

    Cleanup Message example

--- examples/message.js
+++ examples/message.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 'use strict';
 
+// Dependencies
 const Message = require('../types/message');
 
+// Main Process
 async function main () {
+  // Create Message instance
   const message = Message.fromVector(['GenericMessage', '"Hello, world!"']);
+
+  // Return generic Object
   return {
     message: message,
     raw: message.asRaw()
   };
 }
 
+// Execution
 main().catch((exception) => {
   console.error('[EXAMPLES:MESSAGE]', 'Main Process Exception:', exception);
 }).then((output) => {
